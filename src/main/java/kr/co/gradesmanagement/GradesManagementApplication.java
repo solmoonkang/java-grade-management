2bcf5cb4b605a288d50a3e89a27816ddb449a656
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-01 02:36:28 +0900

    init

---
+++ src/main/java/kr/co/gradesmanagement/GradesManagementApplication.java
@@ -0,0 +1,13 @@
+package kr.co.gradesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradesManagementApplication.class, args);
+    }
+
+}
