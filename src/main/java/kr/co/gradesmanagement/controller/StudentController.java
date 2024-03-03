1261155b5faac14e754373506c40dfaff9348f50
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-03 21:39:28 +0900

    feat: 모든 학생을 조회하는 컨트롤러 구현

--- src/main/java/kr/co/gradesmanagement/controller/StudentController.java
+++ src/main/java/kr/co/gradesmanagement/controller/StudentController.java
@@ -1,11 +1,14 @@
 package kr.co.gradesmanagement.controller;
 
 import kr.co.gradesmanagement.dto.request.StudentReqDTO;
+import kr.co.gradesmanagement.dto.response.StudentResDTO;
 import kr.co.gradesmanagement.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -18,5 +21,10 @@ public class StudentController {
     public ResponseEntity<String> createStudent(@RequestBody StudentReqDTO.CREATE create) {
         studentService.createStudent(create);
         return ResponseEntity.status(HttpStatus.CREATED).body("OK");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResDTO.READ>> findAllStudents() {
+        return ResponseEntity.ok(studentService.findAllStudents());
     }
 }
