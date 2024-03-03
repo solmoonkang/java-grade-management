b300e666e2c9700c1ba380bb4d99bc93bcb276c1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-03 20:26:33 +0900

    feat: 학점 범위 유효성 검사

--- src/main/java/kr/co/gradesmanagement/service/StudentService.java
+++ src/main/java/kr/co/gradesmanagement/service/StudentService.java
@@ -1,10 +1,13 @@
 package kr.co.gradesmanagement.service;
 
+import kr.co.gradesmanagement.domain.Grade;
 import kr.co.gradesmanagement.domain.Student;
 import kr.co.gradesmanagement.dto.request.StudentReqDTO;
 import kr.co.gradesmanagement.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +17,25 @@ public class StudentService {
 
     public void createStudent(StudentReqDTO.CREATE create) {
         final Student student = Student.create(create);
-        validateStudentYear(create.getYear());
+        checkStudentGradeExists(String.valueOf(create.getGrade()));
+        checkStudentYearExists(create.getYear());
         studentRepository.save(student);
     }
 
-    private void validateStudentYear(int year) {
+    private void checkStudentYearExists(int year) {
         if (year < 0 || year >= 6) {
-            throw new IllegalArgumentException("⚠️[ERROR] " + year + "는 유효하지 않은 학년입니다. 학년은 1~5학년 중에서 입력해주세요.");
+            throw new IllegalArgumentException("⚠️[ERROR] " + year + "는 유효하지 않은 학년입니다. "
+                    + "학년은 1~5학년 중에서 입력해주세요.");
+        }
+    }
+
+    private void checkStudentGradeExists(String grade) {
+        boolean isMatchGrade = Arrays.stream(Grade.values())
+                .anyMatch(validGrade -> validGrade.name().equals(grade));
+
+        if (!isMatchGrade) {
+            throw new IllegalArgumentException("⚠️[ERROR] " + grade + "는 유효하지 않은 학점입니다. "
+                    + "학점은 A, B, C, D, F 중에서 입력해주세요.");
         }
     }
 }
