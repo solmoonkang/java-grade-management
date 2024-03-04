13d82a1990955b1551aa07e57bfbd2f3831f167a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-04 18:20:00 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/kr/co/gradesmanagement/controller/StudentController.java
+++ src/main/java/kr/co/gradesmanagement/controller/StudentController.java
@@ -7,8 +7,6 @@ import kr.co.gradesmanagement.model.dto.request.StudentReqDTO;
 import kr.co.gradesmanagement.model.dto.response.StudentResDTO;
 import kr.co.gradesmanagement.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
