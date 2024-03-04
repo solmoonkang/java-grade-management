f2c6af5a45c34c2a597fc4195706196a888cc078
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-04 12:41:43 +0900

    feat: 성공적으로 메시지 전송 시 응답 포맷 구현

---
+++ src/main/java/kr/co/gradesmanagement/infra/model/ApiResponse.java
@@ -0,0 +1,31 @@
+package kr.co.gradesmanagement.infra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    private HttpStatus status;
+
+    private String message;
+
+    private Optional<T> data;
+
+    private boolean isSuccessful;
+
+    public static <T> ApiResponse<T> successMessage(ErrorCode errorCode) {
+        return ApiResponse.<T>builder()
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .data(Optional.empty())
+                .isSuccessful(true)
+                .build();
+    }
+}
