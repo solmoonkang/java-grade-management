efab6e62a286e9ae3359d9efd4d5dac82400cf7a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-04 18:11:34 +0900

    feat: 학생 정보 요청 dto 검증 처리

--- src/main/java/kr/co/gradesmanagement/model/dto/request/StudentReqDTO.java
+++ src/main/java/kr/co/gradesmanagement/model/dto/request/StudentReqDTO.java
@@ -1,5 +1,7 @@
 package kr.co.gradesmanagement.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kr.co.gradesmanagement.model.domain.Grade;
 import lombok.*;
 
@@ -11,10 +13,13 @@ public class StudentReqDTO {
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class CREATE {
 
+        @NotBlank(message = "학생 이름을 입력해주세요.")
         private String name;
 
+        @NotBlank(message = "학생 성적을 입력해주세요.")
         private Grade grade;
 
+        @NotNull(message = "학생 학년을 입력해주세요.")
         private int year;
     }
 }
